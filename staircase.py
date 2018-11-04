d5a0083caf43074c31c40e8dafcc37fae2013d03
Thomas Shaddox <wediddoit@gmail.com>
2018-11-03 21:54:51 -0700

    linear version to avoid stack overflow

--- staircase.py
+++ staircase.py
@@ -1,3 +1,7 @@
+"""
+From https://www.youtube.com/watch?v=5o-kdjv7FD0&t=151s
+"""
+
 import timeit
 
 
@@ -5,13 +9,17 @@ def number_of_paths(stair_count, steps, recur=None):
     if recur is None:
         recur = number_of_paths
     paths = 0
-    for step in steps:
-        if step > stair_count:
-            pass  # Can't reach the top because we'll overshoot
-        elif step == stair_count:
+    for step_count in steps:
+        if step_count > stair_count:
+            # We can't reach the top because we'll overshoot
+            pass
+        elif step_count == stair_count:
+            # We reach the top in 1 step!
             paths += 1
-        elif step < stair_count:
-            paths += recur(stair_count - step, steps)
+        elif step_count < stair_count:
+            # We climb up the stairs, then add the number of paths from our new
+            # location.
+            paths += recur(stair_count - step_count, steps)
         else:
             assert False, "something very strange happened"
     return paths
@@ -24,27 +32,38 @@ def make_memoized():
         key = stair_count
         if key in cache:
             return cache[key]
-        # print("cache miss", stair_count)
         result = number_of_paths(stair_count, steps, memoized)
         cache[key] = result
-        # print(stair_count, cache)
         return result
 
     return memoized
 
 
+def linear(stair_count, steps):
+    memoized = make_memoized()
+    result = None
+    for i in range(stair_count + 1):
+        result = memoized(i, steps)
+    return result
+
+
 def main():
-    stair_count = 300
-    steps = [1, 2, 3, 4, 5]
+    stair_count = 200
+    steps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     memoized = make_memoized()
 
     def format_int(i):
         return "{:.2E}".format(i)
 
+    # The simple memoized version will stack overflow at around 500 stairs.
     result = memoized(stair_count, steps)
     print(format_int(result))
-    # print(number_of_paths(stair_count, steps))
+
+    # The linear version uses the memoized version and just pre-caches from
+    # 0 to `stair_count`, so the stack never grows.
+    result = linear(stair_count, steps)
+    print(format_int(result))
 
 
 main()
